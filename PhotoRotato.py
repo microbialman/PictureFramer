adf769e0aea5b80dd7deacc4998c23f2cbbfa188
Matt Jackson-Wood <mattjackson4@gmail.com>
2025-02-12 17:42:42 +0000

    Initial work on face detection - centering doesnt seem to work

--- PhotoRotato.py
+++ PhotoRotato.py
@@ -4,16 +4,17 @@ import os
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 import random
 import time
-from PIL import Image, ExifTags
+from PIL import Image
 import screeninfo
 import pygame
-import io
 import math
 import sys
+import cv2
 
 #Color palette if option to have squares is chosen
 COLOR_PALETTES = {
     "Mondrian":[
+    (243, 243, 242),
     (243, 243, 242),
     (175, 54, 60),
     (56, 61, 150),
@@ -28,18 +29,27 @@ COLOR_PALETTES = {
     ]
 }
 
-DISPLAY_TIME = 2
-SQUARES = True
+DISPLAY_TIME = 3
+#set to "All" for only coloured squares, "Some" for a mix of photos and squares, "None" for only photos
+SQUARES = "Some"
 PALETTE = "Mondrian"
 BORDER_WIDTH = 10
-ANIMATION_SPEED = 20
-TICK_SPEED = 60
+ANIMATION_SPEED = 15
+TICK_SPEED = 30
+#if on initialisation will be slower as faces are detected in each image to center cropping
+FACEDETECT = False
 FULLSCREEN = True
+
+FACEMODEL = cv2.CascadeClassifier(
+    cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
+)
 
 class ImageObject:
     #setup image
-    def __init__(self, img, target_pos, target_size, swid, shei, is_color_square=False, color=None):
-        self.img = img
+    def __init__(self, img, target_pos, target_size, swid, shei, focus, is_color_square=False, color=None):
+        #load and trim the image
+        if is_color_square == False:
+            self.img = scale_and_crop(img, target_size, focus)
         #coorindate to end up at
         self.target_pos = target_pos
         #size in layout
@@ -47,7 +57,7 @@ class ImageObject:
         #store screen size
         self.screendim = (swid, shei)
         #add a slight random speed adjustment to add variety
-        self.speed_adj = random.uniform(0.8,1.1)
+        self.speed_adj = random.uniform(0.8,1.2)
         # set inital position off screen in a horizontal or vertical direciton
         direction = random.choice(["left", "right", "up", "down"])
         if direction == "left":
@@ -99,15 +109,34 @@ class ImageObject:
             pygame.draw.rect(screen, self.color, (pos[0], pos[1], self.target_size[0], self.target_size[1]))
         else:
             #scale and crop to maintain aspect ratio
-            scaled_img = scale_and_crop(self.img, self.target_size)
-            img_rect = scaled_img.get_rect(center=(pos[0] + self.target_size[0] // 2, 
-                                                    pos[1] + self.target_size[1] // 2))
-            screen.blit(scaled_img, img_rect.topleft)
+            screen.blit(self.img, (pos[0], pos[1], self.target_size[0], self.target_size[1]))
         #add border
         pygame.draw.rect(screen, (0, 0, 0), (pos[0], pos[1], self.target_size[0], self.target_size[1]), BORDER_WIDTH)
+
+#function to ID faces in an image
+def find_faces(file, fm=FACEMODEL):
+    img = cv2.imread(file)
+    resizer = 0.3
+    if img.shape[0] and  img.shape[1] <1000:
+        resizer = 1
+    gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    gray_img = cv2.resize(gray_img, (0,0), fx=resizer, fy=resizer)  
+    face = fm.detectMultiScale(
+        gray_img,  minNeighbors=4)
+    #return mean vector
+    if len(face)>0:
+            x_mean = int(sum(x+(w/2) for x, y,  w, h in face) / len(face))
+            y_mean = int(sum(y+(h/2) for x, y,  w, h in face) / len(face))
+            x_mean = x_mean * (1/resizer)
+            y_mean = y_mean * (1/resizer)
+            return(x_mean,y_mean)
+    else:
+        return(False)
             
-#store initial info on images for layout creation
-def get_image_info(folder_path):
+        
+#store initial info on images for layout creation, try to detect faces to find a region to focus on
+def get_image_info(folder_path, F=False):
+    print("Loading images...")
     image_info = {}
     for filename in os.listdir(folder_path):
         file_path = os.path.join(folder_path, filename)
@@ -118,7 +147,8 @@ def get_image_info(folder_path):
             image_info[file_path] = {
                     "width": width,
                     "height": height,
-                    "orientation": "Portrait" if height > width else "Landscape"
+                    "orientation": "Portrait" if height > width else "Landscape",
+                    "focus": find_faces(file_path) if F==True else False
                 }
     return image_info
 
@@ -128,15 +158,22 @@ def get_display_resolution():
     return {"width": mon.width, "height": mon.height}
 
 #for overly large images dont skew dimensions just focus on center of image
-def scale_and_crop(image, target_size):
+def scale_and_crop(image, target_size, focus):
     img_w, img_h = image.get_size()
     target_w, target_h = target_size
     scale = max(target_w / img_w, target_h / img_h)
     new_size = (math.ceil(img_w * scale), math.ceil(img_h * scale))
     image = pygame.transform.smoothscale(image, new_size)
-    # Compute centered crop rectangle of exactly target_size.
-    crop_x = (new_size[0] - target_w) // 2
-    crop_y = (new_size[1] - target_h) // 2
+    #if no faces center on the middle
+    if focus == False:
+        # Compute centered crop rectangle of exactly target_size
+        crop_x = (new_size[0] - target_w) // 2
+        crop_y = (new_size[1] - target_h) // 2
+    else:
+        focusper = (focus[0] // img_w , focus[1] // img_h)
+        focusadj = (new_size[0]*focusper[0], new_size[1]*focusper[1])
+        crop_x = focusadj[0] - (target_w // 2)
+        crop_y = focusadj[1] - (target_h // 2)
     # Clamp crop values to ensure the rectangle is within bounds.
     crop_x = max(0, min(crop_x, new_size[0] - target_w))
     crop_y = max(0, min(crop_y, new_size[1] - target_h))
@@ -155,7 +192,7 @@ def pyg_handle(clock):
                 sys.exit()
 
 #run the slideshow
-def display_slideshow(image_info, display_resolution, interval=3, fullscreen=False, squares=True):
+def display_slideshow(image_info, display_resolution, interval=3, fullscreen=False, squares="Some"):
     pygame.init()
     pygame.mouse.set_visible(False)
     # Set the screen size and windowed or not
@@ -174,30 +211,38 @@ def display_slideshow(image_info, display_resolution, interval=3, fullscreen=Fal
     images = list(image_info.keys())
     random.shuffle(images)
 
+    #load the layouts
+    layouts = get_layouts(display_resolution, laylis)
+    
     running = True
     
     #loop to run the slideshow
+    print("Starting slideshow...")
     while running:
         #get the layout
-        layout = random.choice(get_layouts(display_resolution))
+        layout = random.choice(layouts)
         objects = []
 
         #get images for the layout
         selected_images = random.sample(images, min(len(images), len(layout)))
+            
         #if squares is on on replace some photos with coloured squares, ensure there is always at least image
         num_replacements = 0
-        if squares:
+        if squares == "Some":
             num_replacements = random.randint(0, len(layout) - 1 )
+        elif squares == "All":
+            num_replacements = len(layout)
         replace_indices = random.sample(range(len(layout)), num_replacements)
 
         #add the coloured squares or images
         for i, pos in enumerate(layout):
             if i in replace_indices:
                 color = random.choice(COLOR_PALETTES[PALETTE])
-                objects.append(ImageObject(None, pos["position"], pos["size"], swidth, sheight, is_color_square=True, color=color))
+                objects.append(ImageObject(None, pos["p"], pos["s"], swidth, sheight, False, is_color_square=True, color=color))
             else:
-                img = pygame.image.load(selected_images.pop())
-                objects.append(ImageObject(img, pos["position"], pos["size"], swidth, sheight))
+                ipath = selected_images.pop()
+                img = pygame.image.load(ipath)
+                objects.append(ImageObject(img, pos["p"], pos["s"], swidth, sheight, image_info[ipath]["focus"]))
 
         #animate objects from their random start positions to their layout positions
         while not all(obj.done for obj in objects):
@@ -227,15 +272,40 @@ def display_slideshow(image_info, display_resolution, interval=3, fullscreen=Fal
             pyg_handle(clock)
             
 #specify possible layouts
-def get_layouts(display_resolution):
+def get_layouts(display_resolution, laylis):
     width, height = display_resolution["width"], display_resolution["height"]
-    return [
-        [{"position": (width // 3 * i, 0), "size": (width // 3, height)} for i in range(3)],
-        [{"position": (0, 0), "size": (width, height)}],
-        [{"position": (x * width // 2, y * height // 2), "size": (width // 2, height // 2)} for y in range(2) for x in range(2)]
-    ]
+    for i in laylis:
+        for j in i:
+            j["p"][0] = j["p"][0] * width
+            j["p"][1] = j["p"][1] * height
+            j["s"][0] = j["s"][0] * width
+            j["s"][1] = j["s"][1] * height
+    return laylis
+
+#list with possible layouts, p gives top left position (x,y) , s gives size (w,h) , all values are fractions of total screen
+laylis = [
+    [{"p":[0,0],"s":[1,1]}],
+    [{"p":[0,0],"s":[1,1]}],
+    [{"p":[0,0],"s":[1,1]}],
+    [{"p":[0,0],"s":[0.4,1]}, {"p":[0.4,0],"s":[0.6,0.5]}, {"p":[0.4,0.5],"s":[0.6,0.5]}],
+    [{"p":[0,0],"s":[0.6,0.5]}, {"p":[0,0.5],"s":[0.6,0.5]}, {"p":[0.6,0],"s":[0.4,1]}],
+    [{"p":[0,0],"s":[0.5,0.5]},{"p":[0.5,0],"s":[0.5,0.5]},{"p":[0,0.5],"s":[0.5,0.5]},{"p":[0.5,0.5],"s":[0.5,0.5]}],
+    [{"p":[0,0],"s":[0.4,0.5]},{"p":[0,0.5],"s":[0.4,0.5]},{"p":[0.4,0],"s":[0.3,1]},{"p":[0.7,0],"s":[0.3,1]}],
+    [{"p":[0,0],"s":[0.3,1]},{"p":[0.3,0],"s":[0.3,1]},{"p":[0.6,0],"s":[0.4,0.5]},{"p":[0.6,0.5],"s":[0.4,0.5]}],
+    [{"p":[0,0],"s":[0.333,1]},{"p":[0.333,0],"s":[0.333,1]},{"p":[0.666,0],"s":[0.333,1]}],
+    [{"p":[0,0],"s":[0.3,1]},{"p":[0.3,0],"s":[0.4,0.5]},{"p":[0.3,0.5],"s":[0.4,0.5]},{"p":[0.7,0],"s":[0.3,1]}],
+    [{"p":[0,0],"s":[0.3,0.5]},{"p":[0,0.5],"s":[0.3,0.5]},{"p":[0.3,0],"s":[0.4,1]},{"p":[0.7,0],"s":[0.3,0.5]},{"p":[0.7,0.5],"s":[0.3,0.5]}],
+    [{"p":[0,0],"s":[0.7,1]},{"p":[0.7,0],"s":[0.3,0.4]},{"p":[0.7,0.4],"s":[0.3,0.6]}],
+    [{"p":[0,0],"s":[0.3,0.6]},{"p":[0,0.6],"s":[0.3,0.4]},{"p":[0.3,0],"s":[0.7,1]}],
+    [{"p":[0,0],"s":[0.25,1]},{"p":[0.25,0],"s":[0.5,1]},{"p":[0.75,0],"s":[0.25,1]}],
+    [{"p":[0,0],"s":[0.5,0.5]},{"p":[0,0.5],"s":[0.5,0.5]},{"p":[0.5,0],"s":[0.5,1]}],
+    [{"p":[0,0],"s":[0.5,1]},{"p":[0.5,0],"s":[0.5,0.5]},{"p":[0.5,0.5],"s":[0.5,0.5]}],
+    [{"p":[0,0],"s":[0.4,0.333]},{"p":[0,0.333],"s":[0.4,0.333]},{"p":[0,0.666],"s":[0.4,0.333]},{"p":[0.4,0],"s":[0.6,1]}],
+    [{"p":[0,0],"s":[0.6,1]},{"p":[0.6,0],"s":[0.4,0.333]},{"p":[0.6,0.333],"s":[0.4,0.333]},{"p":[0.6,0.666],"s":[0.4,0.333]}],
+    [{"p":[0,0],"s":[0.6,0.6]},{"p":[0,0.6],"s":[0.3,0.4]},{"p":[0.3,0.6],"s":[0.3,0.4]},{"p":[0.6,0],"s":[0.4,0.3]},{"p":[0.6,0.3],"s":[0.4,0.7]}]
+]
 
 folder_path = "Images"
-image_data = get_image_info(folder_path)
+image_data = get_image_info(folder_path, F=FACEDETECT)
 display_resolution = get_display_resolution()
 display_slideshow(image_data, display_resolution, interval=DISPLAY_TIME, fullscreen=FULLSCREEN, squares=SQUARES)
--- convert_to_jpeg.py
+++ convert_to_jpeg.py
@@ -1,6 +1,6 @@
 import os
 import pillow_heif
-from PIL import Image, ExifTags
+from PIL import Image, ExifTags, ImageOps
 
 def convert_images_to_jpeg(folder):
     # Ensure the folder exists
@@ -14,42 +14,28 @@ def convert_images_to_jpeg(folder):
         if os.path.isfile(filepath):
             name, ext = os.path.splitext(filename)
             ext = ext.lower()
-            
-            if ext in ['.jpg', '.jpeg']:
-                continue  # Skip JPEG files
-            
+                       
             try:
-                if ext == '.heic':
-                    heif_image = pillow_heif.open_heif(filepath)
-                    image = Image.frombytes(
-                        heif_image.mode, heif_image.size, heif_image.data, "raw", heif_image.mode, 0, 1
-                    )
-                else:
-                    image = Image.open(filepath)
-                
-                # Handle orientation using EXIF data
-                try:
-                    for orientation in ExifTags.TAGS.keys():
-                        if ExifTags.TAGS[orientation] == 'Orientation':
-                            break
-                    exif = image._getexif()
-                    if exif and orientation in exif:
-                        if exif[orientation] == 3:
-                            image = image.rotate(180, expand=True)
-                        elif exif[orientation] == 6:
-                            image = image.rotate(270, expand=True)
-                        elif exif[orientation] == 8:
-                            image = image.rotate(90, expand=True)
-                except (AttributeError, KeyError, IndexError):
-                    pass  # No EXIF orientation data
-                
-                # Convert and replace the original file
+                image = Image.open(filepath)    
+                image = ImageOps.exif_transpose(image)
+                #replace the original file
+                os.remove(filepath)  # Remove original file
                 new_filepath = os.path.join(folder, f"{name}.jpg")
                 image.save(new_filepath, "JPEG", quality=95)
-                os.remove(filepath)  # Remove original file
                 print(f"Converted: {filename} -> {name}.jpg")
-            except Exception as e:
-                print(f"Error processing {filename}: {e}")
+
+            except:
+                try:
+                    heif_image = pillow_heif.open_heif(filepath)
+                    image = Image.frombytes(heif_image.mode, heif_image.size, heif_image.data, "raw", heif_image.mode, 0, 1)
+                    #replace the original file
+                    os.remove(filepath)  # Remove original file
+                    new_filepath = os.path.join(folder, f"{name}.jpg")
+                    image.save(new_filepath, "JPEG", quality=95)
+                    print(f"Converted: {filename} -> {name}.jpg")
+
+                except Exception as e:
+                    print(f"Error processing {filename}: {e}")
 
 if __name__ == "__main__":
     convert_images_to_jpeg("Images")
